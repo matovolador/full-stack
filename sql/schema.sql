5a884de967ba75d14b27505071860bf955586ad9
Matias Garafoni <matias.garafoni@gmail.com>
2022-02-22 17:30:56 -0300

    no longer using this

--- sql/schema.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, first_name varchar(250), last_name VARCHAR(250),  email VARCHAR(200) NOT NULL UNIQUE, passcode VARCHAR(100), created TIMESTAMP NOT NULL DEFAULT NOW(), passcode_created TIMESTAMP, last_seen TIMESTAMP DEFAULT now());
-
-
-INSERT INTO users (first_name,last_name,email,passcode,passcode_created,last_seen) VALUES ('Matias','Garafoni','matias.garafoni@gmail.com',12345,NOW(),NOW());
