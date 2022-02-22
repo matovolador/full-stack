885a89232cfcd627fad54d81d359421bcc5017ee
Matias Garafoni <matias.garafoni@gmail.com>
2022-02-22 17:24:33 -0300

    use url from .env

--- alembic/env.py
+++ alembic/env.py
@@ -14,6 +14,7 @@ load_dotenv()
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option('sqlalchemy.url', os.getenv("DATABASE_URL"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
